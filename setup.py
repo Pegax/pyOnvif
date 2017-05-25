00297fb92d50fd0a7b486dea6ce73ff5eef5eb1c
Petri Savolainen <petri.savolainen@koodaamo.fi>
2017-05-25 16:19:48 +0300

    package for PyPI

---
+++ setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup
+
+
+def get_file(fn):
+    with open(fn) as fp:
+        return fp.read()
+
+setup(
+    name='pyOnvif',
+    version="1.0",
+    description='Simple Onvif camera client',
+    long_description=(
+        get_file('README.rst') + '\n\n' + get_file('HISTORY.rst')
+    ),
+    author="Pekka Jäppinen",
+    author_email='',
+    url='https://github.com/Pegax/pyOnvif',
+    install_requires=[],
+    packages=[
+        'pyonvif',
+    ],
+    package_dir={
+        'pyonvif': 'src'
+    },
+    include_package_data=True,
+    license="GPLv3",
+    zip_safe=False,
+    keywords='onvif',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)'
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.7',
+        "Programming Language :: Python :: 3",
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
