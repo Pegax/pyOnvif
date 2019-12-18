6a11a7ec1b58e834286871af952ba0ad8b047aa6
Petri Savolainen <petri@koodaamo.fi>
2019-12-18 19:55:46 +0200

    refactor namespaces into its own module

--- pyonvif/pyonvif.py
+++ pyonvif/pyonvif.py
@@ -14,7 +14,7 @@ import string
 import base64
 import http.client
 from urllib.parse import urlparse
-
+from .namespaces import *
 from wsdiscovery import WSDiscovery
 
 
@@ -77,14 +77,14 @@ class OnvifCam():
 
   # Communication with camera
 
-  def Onvifdiscovery(self, retries=3):
+  def discoverServices(self, retries=3):
     "Use WS DIscovery to try & find available camera(s) a number of times"
     wsd = WSDiscovery()
     wsd.start()
     self.camlist=[]
     resp=[]
     a=0
-    while not resp and a<retries:
+    while not resp and a < retries:
       ret = wsd.searchServices()
       a=a+1
       for service in ret:
@@ -107,16 +107,16 @@ class OnvifCam():
   # SOAP handling utility functions
 
   def insertInEnvelope(self, msg):
-    resp='<s:Envelope xmlns:s="http://www.w3.org/2003/05/soap-envelope">%s</s:Envelope>' % (msg)
+    resp = f'<s:Envelope xmlns:s="{SOAP_ENV}">{msg}</s:Envelope>'
     return resp
 
   def insertInBody(self, msg):
-    resp='<s:Body xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema">%s</s:Body>' % (msg)
+    resp = f'<s:Body xmlns:xsi="{XML_SCHEMA_INSTANCE}" xmlns:xsd="{XML_SCHEMA}">{msg}</s:Body>'
     return resp
 
   def sendSoapMsg(self, bmsg):
     if self.header:
-      fullmsg= '%s%s' % (self.onvifauthheader(),self.insertInBody(bmsg))
+      fullmsg= f'%s%s' % (self.onvifauthheader(),self.insertInBody(bmsg))
       soapmsg=self.insertInEnvelope(fullmsg)
     else:
       soapmsg=self.insertInEnvelope(self.insertInBody(bmsg))
@@ -127,126 +127,126 @@ class OnvifCam():
   def onvifauthheader(self):
     created = datetime.datetime.now().isoformat().split(".")[0]
     pool = string.ascii_letters + string.digits + string.punctuation
-    n64 = ''.join(SystemRandom().choice(pool) for _ in range(22))
+    n64 = f''.join(SystemRandom().choice(pool) for _ in range(22))
     nonce = base64.b64encode(n64.encode('ascii')).decode("ascii")
     base = (n64 + created + self.password).encode("ascii")
     pdigest= base64.b64encode(sha1(base).digest()).decode("ascii")
     username='<Username>%s</Username>' % self.username
-    password= '<Password Type="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-username-token-profile-1.0#PasswordDigest">%s</Password>' % pdigest
-    Nonce = '<Nonce EncodingType="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-soap-message-security-1.0#Base64Binary">%s</Nonce>' % nonce
-    created = '<Created xmlns="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd">%s</Created>' % created
-    usertoken= '<UsernameToken>%s%s%s%s</UsernameToken>' % (username, password, Nonce, created)
-    header = '<s:Header><Security s:mustUnderstand="1" xmlns="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd">%s</Security></s:Header>' % usertoken
+    password= f'<Password Type="{WSS_PWDIGEST}">{pdigest}</Password>'
+    Nonce = f'<Nonce EncodingType="{WSS_BASE&4BIN}">%s</Nonce>' % nonce
+    created = f'<Created xmlns="{WSS_SECUTIL}">%s</Created>' % created
+    usertoken= f'<UsernameToken>%s%s%s%s</UsernameToken>' % (username, password, Nonce, created)
+    header = f'<s:Header><Security s:mustUnderstand="1" xmlns="{WSS_SECEXT}">%s</Security></s:Header>' % usertoken
     return header
 
 
   # Onvif messages
 
   def getSystemDateAndTime(self):
-    bmsg = '<GetSystemDateAndTime xmlns="http://www.onvif.org/ver10/device/wsdl"/>'
+    bmsg = f'<GetSystemDateAndTime xmlns="OVF_DEVICE"/>'
     return self.sendSoapMsg(bmsg)
 
   def getCapabilities(self):
-    bmsg = '<GetCapabilities xmlns="http://www.onvif.org/ver10/device/wsdl"><Category>All</Category></GetCapabilities>'
+    bmsg = f'<GetCapabilities xmlns="OVF_DEVICE"><Category>All</Category></GetCapabilities>'
     return self.sendSoapMsg(bmsg)
 
   def getServiceCapabilities(self):
-    bmsg = '<GetServiceCapabilities xmlns="http://www.onvif.org/ver10/device/wsdl"></GetServiceCapabilities>'
+    bmsg = f'<GetServiceCapabilities xmlns="OVF_DEVICE"></GetServiceCapabilities>'
     return self.sendSoapMsg(bmsg)
 
   def getServices(self):
-    bmsg='<GetServices xmlns="http://www.onvif.org/ver10/device/wsdl"><IncludeCapability>false</IncludeCapability></GetServices>'
+    bmsg='<GetServices xmlns="OVF_DEVICE"><IncludeCapability>false</IncludeCapability></GetServices>'
     return self.sendSoapMsg(bmsg)
 
   def getProfiles(self):
-    bmsg='<GetProfiles xmlns="http://www.onvif.org/ver10/media/wsdl"/>'
+    bmsg='<GetProfiles xmlns="{OVF_MEDIA}"/>'
     self.profiles=[]
     return self.sendSoapMsg(bmsg)
 
   def getDeviceInformation(self):
-    bmsg='<GetDeviceInformation xmlns="http://www.onvif.org/ver10/device/wsdl"/>'
+    bmsg='<GetDeviceInformation xmlns="OVF_DEVICE"/>'
     return self.sendSoapMsg(bmsg)
 
   def getNode(self,nodetoken):
-    bmsg='<GetNode xmlns="http://www.onvif.org/ver20/ptz/wsdl"><NodeToken>%s</NodeToken></GetNode>' % nodetoken
+    bmsg='<GetNode xmlns="{OVF_PTZ}"><NodeToken>%s</NodeToken></GetNode>' % nodetoken
     return self.sendSoapMsg(bmsg)
 
   # CAMERA MOVEMENTS
 
   def relativeMove(self, x, y, xspeed="0.5", yspeed="0.5"):
-    profile = '<ProfileToken>%s</ProfileToken>' % (self.profiletoken)
-    pantilt = '<PanTilt x="%s" y="%s" space="http://www.onvif.org/ver10/tptz/PanTiltSpaces/TranslationGenericSpace" xmlns="http://www.onvif.org/ver10/schema"/>' % (x, y)
-    pantilts ='<PanTilt x="%s" y="%s" space="http://www.onvif.org/ver10/tptz/PanTiltSpaces/GenericSpeedSpace" xmlns="http://www.onvif.org/ver10/schema"/>' % (xspeed,yspeed)
-    bmsg='<RelativeMove xmlns="http://www.onvif.org/ver20/ptz/wsdl">%s<Translation>%s</Translation><Speed>%s</Speed></RelativeMove>' % (profile, pantilt, pantilts )
+    profile = f'<ProfileToken>%s</ProfileToken>' % (self.profiletoken)
+    pantilt = f'<PanTilt x="%s" y="%s" space="{OVF_PTS_TGS}" xmlns="{OVF_SCHEMA}"/>' % (x, y)
+    pantilts ='<PanTilt x="%s" y="%s" space="{OVF_PTS_GSS}" xmlns="{OVF_SCHEMA}"/>' % (xspeed,yspeed)
+    bmsg='<RelativeMove xmlns="{OVF_PTZ}">%s<Translation>%s</Translation><Speed>%s</Speed></RelativeMove>' % (profile, pantilt, pantilts )
     return self.sendSoapMsg(bmsg)
 
   def relativeMoveZoom(self, z, zspeed="0.5"):
-    profile = '<ProfileToken>%s</ProfileToken>' % (self.profiletoken)
-    zoom = '<Zoom x="%s" space="http://www.onvif.org/ver10/tptz/ZoomSpaces/TranslationGenericSpace" xmlns="http://www.onvif.org/ver10/schema"/>' % (z)
-    zoomspeed ='<Speed><Zoom x="%s" space="http://www.onvif.org/ver10/tptz/ZoomSpaces/ZoomGenericSpeedSpace" xmlns="http://www.onvif.org/ver10/schema"/></Speed>' % (zspeed)
-    bmsg= '<RelativeMove xmlns="http://www.onvif.org/ver20/ptz/wsdl">%s<Translation>%s</Translation>%s</RelativeMove>' % (profile, zoom, zoomspeed)
+    profile = f'<ProfileToken>%s</ProfileToken>' % (self.profiletoken)
+    zoom = f'<Zoom x="%s" space="{OVF_ZS_TGS}" xmlns="{OVF_SCHEMA}"/>' % (z)
+    zoomspeed ='<Speed><Zoom x="%s" space="{OVF_ZS_ZGSS}" xmlns="{OVF_SCHEMA}"/></Speed>' % (zspeed)
+    bmsg = f'<RelativeMove xmlns="{OVF_PTZ}">%s<Translation>%s</Translation>%s</RelativeMove>' % (profile, zoom, zoomspeed)
     return self.sendSoapMsg(bmsg)
 
   def absoluteMove(self, x, y, z):
-    profile = '<ProfileToken>%s</ProfileToken>' % (self.profiletoken)
-    pantilt = '<PanTilt x="%s" y="%s" space="http://www.onvif.org/ver10/tptz/PanTiltSpaces/PositionGenericSpace" xmlns="http://www.onvif.org/ver10/schema"/>' % (x,y)
-    zoom = '<Zoom x="%s" space="http://www.onvif.org/ver10/tptz/ZoomSpaces/PositionGenericSpace" xmlns="http://www.onvif.org/ver10/schema"/>' % (z)
-    bmsg='<AbsoluteMove xmlns="http://www.onvif.org/ver20/ptz/wsdl"><Position>%s%s</Position></AbsoluteMove>' % (profile,pantilt,zoom)
+    profile = f'<ProfileToken>%s</ProfileToken>' % (self.profiletoken)
+    pantilt = f'<PanTilt x="%s" y="%s" space="{OVF_PTS_PGS}" xmlns="{OVF_SCHEMA}"/>' % (x,y)
+    zoom = f'<Zoom x="%s" space="{OVF_ZS_PGS}" xmlns="{OVF_SCHEMA}"/>' % (z)
+    bmsg='<AbsoluteMove xmlns="{OVF_PTZ}"><Position>%s%s</Position></AbsoluteMove>' % (profile,pantilt,zoom)
     return self.sendSoapMsg(bmsg)
 
   def continuousMove(self, x, y):
-    profile = '<ProfileToken>%s</ProfileToken>' % (self.profiletoken)
-    pantilt = '<PanTilt x="%s" y="%s" space="http://www.onvif.org/ver10/tptz/PanTiltSpaces/VelocityGenericSpace" xmlns="http://www.onvif.org/ver10/schema"/>' % (x,y)
-    bmsg = ' <ContinuousMove xmlns="http://www.onvif.org/ver20/ptz/wsdl">%s<Velocity>%s</Velocity></ContinuousMove>' % (profile, pantilt)
+    profile = f'<ProfileToken>%s</ProfileToken>' % (self.profiletoken)
+    pantilt = f'<PanTilt x="%s" y="%s" space="{OVF_PTS_VGS}" xmlns="{OVF_SCHEMA}"/>' % (x,y)
+    bmsg = f' <ContinuousMove xmlns="{OVF_PTZ}">%s<Velocity>%s</Velocity></ContinuousMove>' % (profile, pantilt)
     return self.sendSoapMsg(bmsg)
 
   def stopMove(self, ptstop="false", zstop="false"):
-    profile = '<ProfileToken>%s</ProfileToken>' % (self.profiletoken)
-    pantilt = '<PanTilt>%s</PanTilt>'%ptstop
-    zoom = '<Zoom>%s</Zoom>' % zstop
-    bmsg = '<Stop xmlns="http://www.onvif.org/ver20/ptz/wsdl">%s%s%s</Stop>' % (profile, pantilt, zoom)
+    profile = f'<ProfileToken>%s</ProfileToken>' % (self.profiletoken)
+    pantilt = f'<PanTilt>%s</PanTilt>'%ptstop
+    zoom = f'<Zoom>%s</Zoom>' % zstop
+    bmsg = f'<Stop xmlns="{OVF_PTZ}">%s%s%s</Stop>' % (profile, pantilt, zoom)
     return self.sendSoapMsg(bmsg)
 
   def continuousZoom(self, z):
-    profile = '<ProfileToken>%s</ProfileToken>' % (self.profiletoken)
-    zoom = '<Zoom x="%s" space="http://www.onvif.org/ver10/tptz/ZoomSpaces/VelocityGenericSpace" xmlns="http://www.onvif.org/ver10/schema"/>' % (z)
-    bmsg = '<ContinuousMove xmlns="http://www.onvif.org/ver20/ptz/wsdl">%s<Velocity>%s</Velocity></ContinuousMove>' % (profile, zoom)
+    profile = f'<ProfileToken>{self.profiletoken}</ProfileToken>'
+    zoom = f'<Zoom x="{z}" space="{OVF_ZS_VGS}" xmlns="{OVF_SCHEMA}"/>'
+    bmsg = f'<ContinuousMove xmlns="{OVF_PTZ}">{profile}<Velocity>{zoom}</Velocity></ContinuousMove>'
     return self.sendSoapMsg(bmsg)
 
   # CAMERA preset use and manipulation
 
   def setPreset(self, presetname):
     "give name for current camera zoomlevel and direction"
-    profile = '<ProfileToken>%s</ProfileToken>' % (self.profiletoken)
-    preset  = '<PresetName%s</PresetName>' % (presetname)
-    bmsg = '<SetPreset xmlns="http://www.onvif.org/ver20/ptz/wsdl"></SetPreset>' % (profile, preset)
+    profile = f'<ProfileToken>%s</ProfileToken>' % (self.profiletoken)
+    preset = f'<PresetName%s</PresetName>' % (presetname)
+    bmsg = f'<SetPreset xmlns="{OVF_PTZ}"></SetPreset>' % (profile, preset)
 
   def getPresets(self):
-    bmsg= '<GetPresets xmlns="http://www.onvif.org/ver20/ptz/wsdl"><ProfileToken>%s</ProfileToken></GetPresets>' % (self.profiletoken)
+    bmsg = f'<GetPresets xmlns="{OVF_PTZ}"><ProfileToken>%s</ProfileToken></GetPresets>' % (self.profiletoken)
     soapmsg=self.insertInEnvelope(self.insertInBody(bmsg))
     self.conn.request("POST", self.cpath, soapmsg)
     return self.conn.getresponse().read()
 
   def gotoPreset(self, presettoken, xspeed="0.5", yspeed="0.5", zspeed="0.5"):
-    profile = '<ProfileToken>%s</ProfileToken>' % (self.profiletoken)
-    preset  = '<PresetToken>%s</PresetToken>' % (presettoken)
-    pantiltspeed= '<PanTilt x="%s" y="%s" xmlns="http://www.onvif.org/ver10/schema"/>' % (xspeed, yspeed)
-    speeddetail = '<Speed>%s<Zoom x="%s" xmlns="http://www.onvif.org/ver10/schema"/></Speed>' % (pantiltspeed, zspeed)
-    bmsg = '<GotoPreset xmlns="http://www.onvif.org/ver20/ptz/wsdl">%s%s%s</GotoPreset>' % (profile, preset, speeddetail)
+    profile = f'<ProfileToken>%s</ProfileToken>' % (self.profiletoken)
+    preset = f'<PresetToken>%s</PresetToken>' % (presettoken)
+    pantiltspeed= f'<PanTilt x="%s" y="%s" xmlns="{OVF_SCHEMA}"/>' % (xspeed, yspeed)
+    speeddetail = f'<Speed>%s<Zoom x="%s" xmlns="{OVF_SCHEMA}"/></Speed>' % (pantiltspeed, zspeed)
+    bmsg = f'<GotoPreset xmlns="{OVF_PTZ}">%s%s%s</GotoPreset>' % (profile, preset, speeddetail)
 
   def removePreset(self, presettoken):
-    profile = '<ProfileToken>%s</ProfileToken>' % (self.profiletoken)
-    preset  = '<PresetToken>%s</PresetToken>' % (presettoken)
-    bmsg = '<RemovePreset xmlns="http://www.onvif.org/ver20/ptz/wsdl">%s%s</RemovePreset>' % (profile, preset)
+    profile = f'<ProfileToken>%s</ProfileToken>' % (self.profiletoken)
+    preset = f'<PresetToken>%s</PresetToken>' % (presettoken)
+    bmsg = f'<RemovePreset xmlns="{OVF_PTZ}">%s%s</RemovePreset>' % (profile, preset)
 
   def getVideoSources(self):
-    bmsg= '<GetVideoSources xmlns="http://www.onvif.org/ver10/media/wsdl"/>'
+    bmsg = f'<GetVideoSources xmlns="{OVF_MEDIA}"/>'
     return self.sendSoapMsg(bmsg)
 
   def getStreamUri(self):
-    stream = '<Stream xmlns="http://www.onvif.org/ver10/schema">RTP-Unicast</Stream>'
-    protocol = '<Protocol>UDP</Protocol>'
-    transport='<Transport xmlns="http://www.onvif.org/ver10/schema">%s</Transport>' % (protocol)
-    streamsetup= '<StreamSetup>%s%s</StreamSetup>' % (stream, transport)
-    bmsg= '<GetStreamUri xmlns="http://www.onvif.org/ver10/media/wsdl">%s<ProfileToken>%s</ProfileToken></GetStreamUri>' % (streamsetup, self.profiletoken)
+    stream = f'<Stream xmlns="{OVF_SCHEMA}">RTP-Unicast</Stream>'
+    protocol = f'<Protocol>UDP</Protocol>'
+    transport='<Transport xmlns="{OVF_SCHEMA}">%s</Transport>' % (protocol)
+    streamsetup= f'<StreamSetup>%s%s</StreamSetup>' % (stream, transport)
+    bmsg = f'<GetStreamUri xmlns="{OVF_MEDIA}">%s<ProfileToken>%s</ProfileToken></GetStreamUri>' % (streamsetup, self.profiletoken)
     return self.sendSoapMsg(bmsg)
